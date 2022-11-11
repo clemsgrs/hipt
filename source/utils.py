549580f1378d39e3de9f90701ca20b4bffaff487
clemsgrs <clementnet@outlook.com>
2022-11-11 17:51:30 +0100

    fixed upsample warning

--- source/utils.py
+++ source/utils.py
@@ -78,7 +78,7 @@ def create_train_tune_test_df(
     return train_df, tune_df, test_df
 
 
-def epoch_time(start_time, end_time):
+def compute_time(start_time, end_time):
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
