53a23ba7fb57d5e5b312d37e352a8b684db02a37
clemsgrs <clementnet@outlook.com>
2023-01-12 15:47:29 +0100

    improved a bit loss documentation

--- source/components.py
+++ source/components.py
@@ -5,19 +5,35 @@ from typing import Optional
 from omegaconf import DictConfig
 
 
-def nll_loss(hazards, survival, label, c, alpha=0.4, eps=1e-7):
-    batch_size = len(label)
-    label = label.view(batch_size, 1) # ground truth bin, 1,2,...,k
+def nll_loss(hazards, survival, Y, c, alpha=0.4, eps=1e-7):
+    '''
+    Continuous time scale divided into k discrete bins: T_cont \in {[0, a_1), [a_1, a_2), ...., [a_(k-1), inf)}
+    Y = T_discrete is the discrete event time:
+        - Y = -1 if T_cont \in (-inf, 0)
+        - Y = 0 if T_cont \in [0, a_1)
+        - Y = 1 if T_cont in [a_1, a_2)
+        - ...
+        - Y = k-1 if T_cont in [a_(k-1), inf)
+    hazards = discrete hazards, hazards(t) = P(Y=t | Y>=t, X) for t = -1, 0, 1, 2, ..., k-1
+    survival = survival function, survival(t) = P(Y > t | X)
+
+    All patients are alive from (-inf, 0) by definition, so P(Y=-1) = 0
+    -> hazards(-1) = 0
+    -> survival(-1) = P(Y > -1 | X) = 1
+
+    Summary: 
+        - neural network is hazard probability function, h(t) for t = 0, 1, 2, ..., k-1
+        - h(t) represents the probability that patient dies in [0, a_1), [a_1, a_2), ..., [a_(k-1), inf]
+    '''
+    batch_size = len(Y)
+    Y = Y.view(batch_size, 1) # ground truth bin, 0, 1, 2, ..., k-1
     c = c.view(batch_size, 1).float() # censorship status, 0 or 1
     if survival is None:
-        survival = torch.cumprod(1 - hazards, dim=1) # surival is cumulative product of 1 - hazards
-    # without padding, survival(0) = survival[0], h(0) = h[0]
-    survival_padded = torch.cat([torch.ones_like(c), survival], 1) #survival(-1) = 0, all patients are alive from (-inf, 0) by definition
-    # after padding, survival(0) = survival[1], survival(1) = survival[2], etc, h(0) = h[0]
-    # h[y] = h(1)
-    # survival[1] = survival(1)
-    uncensored_loss = -(1 - c) * (torch.log(torch.gather(survival_padded, 1, label).clamp(min=eps)) + torch.log(torch.gather(hazards, 1, label).clamp(min=eps)))
-    censored_loss = - c * torch.log(torch.gather(survival_padded, 1, label+1).clamp(min=eps))
+        survival = torch.cumprod(1 - hazards, dim=1) # survival is cumulative product of 1 - hazards
+    survival_padded = torch.cat([torch.ones_like(c), survival], 1) # survival(-1) = 1, all patients are alive from (-inf, 0) by definition
+    # after padding, survival(t=-1) = survival[0], survival(t=0) = survival[1], survival(t=1) = survival[2], etc
+    uncensored_loss = -(1 - c) * (torch.log(torch.gather(survival_padded, 1, Y).clamp(min=eps)) + torch.log(torch.gather(hazards, 1, Y).clamp(min=eps)))
+    censored_loss = - c * torch.log(torch.gather(survival_padded, 1, Y+1).clamp(min=eps))
     neg_l = censored_loss + uncensored_loss
     loss = (1-alpha) * neg_l + alpha * uncensored_loss
     loss = loss.mean()
