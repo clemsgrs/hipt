fixed silent bug in fetching patches from WSI in PatchDataset when wsi object created in init + changed std from statistics.stdev to numpy.std

source/dataset.py
@@ -1049,10 +1049,10 @@ class ImageFolderWithNameDataset(datasets.ImageFolder):
 
 class PatchDataset(torch.utils.data.Dataset):
     def __init__(self, wsi_fp, coordinates, backend):
-        _, _, patch_size_resized, patch_level, factor = coordinates[0]
-        self.coordinates =coordinates[:, :2]
-        self.wsi = wsd.WholeSlideImage(wsi_fp, backend=backend)
-        self.patch_spacing = self.wsi.spacings[patch_level]
+        self.wsi_fp = wsi_fp
+        self.backend = backend
+        _, _, patch_size_resized, self.patch_level, factor = coordinates[0]
+        self.coordinates = coordinates[:, :2]
         self.resize = (factor != 1)
         self.width = patch_size_resized
         self.height = patch_size_resized
@@ -1063,8 +1063,10 @@ class PatchDataset(torch.utils.data.Dataset):
         return len(self.coordinates)
 
     def __getitem__(self, idx):
+        wsi = wsd.WholeSlideImage(self.wsi_fp, backend=self.backend)
         x, y = self.coordinates[idx]
-        patch = self.wsi.get_patch(x, y, self.width, self.height, spacing=self.patch_spacing, center=False)
+        patch_spacing = wsi.spacings[self.patch_level]
+        patch = wsi.get_patch(x, y, self.width, self.height, spacing=patch_spacing, center=False)
         pil_patch = Image.fromarray(patch).convert("RGB")
         if self.resize:
             pil_patch = pil_patch.resize((self.target_width, self.target_height))

train/classification_multi.py
@@ -7,7 +7,6 @@ import torch
 import hydra
 import datetime
 import matplotlib
-import statistics
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
@@ -554,7 +553,7 @@ def main(cfg: DictConfig):
         for metric_name, metric_values in metrics.items()
     }
     std_tune_metrics = {
-        metric_name: round(statistics.stdev(metric_values), 5)
+        metric_name: round(np.std(metric_values), 5)
         for metric_name, metric_values in metrics.items()
     }
     for name in metrics.keys():
@@ -577,7 +576,7 @@ def main(cfg: DictConfig):
         for metric_name, metric_values in metrics.items()
     }
     std_test_metrics = {
-        metric_name: round(statistics.stdev(metric_values), 5)
+        metric_name: round(np.std(metric_values), 5)
         for metric_name, metric_values in metrics.items()
     }
     for name in metrics.keys():

train/survival_multi.py
@@ -5,7 +5,6 @@ import wandb
 import torch
 import hydra
 import datetime
-import statistics
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
@@ -284,9 +283,9 @@ def main(cfg: DictConfig):
                 tune_metrics.append(v)
                 v = round(v, 5)
             if cfg.wandb.enable and r in log_to_wandb["tune"]:
-                wandb.log({f"tune/fold_{i}/{r}": v})
+                wandb.log({f"tune/fold_{i}/{r}_best": v})
             elif "cm" not in r:
-                print(f"tune {r}: {v}")
+                print(f"best tune {r}: {v}")
 
         if do_test:
             test_results = test_survival(
@@ -308,7 +307,7 @@ def main(cfg: DictConfig):
                     print(f"test {r}: {v}")
 
     mean_tune_metric = round(np.mean(tune_metrics), 5)
-    std_tune_metric = round(statistics.stdev(tune_metrics), 5)
+    std_tune_metric = round(np.std(tune_metrics), 5)
     if cfg.wandb.enable and "c-index" in log_to_wandb["tune"]:
         wandb.log({f"tune/c-index_mean": mean_tune_metric})
         wandb.log({f"tune/c-index_std": std_tune_metric})
@@ -316,7 +315,7 @@ def main(cfg: DictConfig):
     if do_test:
 
         mean_test_metric = round(np.mean(test_metrics), 5)
-        std_test_metric = round(statistics.stdev(test_metrics), 5)
+        std_test_metric = round(np.std(test_metrics), 5)
         if cfg.wandb.enable and "c-index" in log_to_wandb["test"]:
             wandb.log({f"test/c-index_mean": mean_test_metric})
             wandb.log({f"test/c-index_std": std_test_metric})